15ef6c1af559ff6cbd0f615af9fa13cc540db578
Tayomide <tibrahim@hawk.iit.edu>
2022-10-30 16:38:34 -0500

    Solve problem three of React Code Challenges

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -4,9 +4,25 @@ export default function FormValidator () {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [messages, setMessages] = useState([])
+
+  const formValidator = () => {
+    const messages = []
+    if(email.length === 0)messages.push("Email feild is required")
+    if(password.length < 8)messages.push("Password must be at least 8 characters long")
+    if(passwordConfirm.length < 8)messages.push("Password Confirmation must be at least 8 characters long")
+    if(passwordConfirm !== password)messages.push("Password confirmation is not the same as password")
+    if(email.split("@").length !== 2)messages.push("An email must have exactly one @ symbol")
+    messages.length? setMessages(messages) : setMessages(["User created!"])
+  }
+
+  const validateForm = (e) => {
+    e.preventDefault()
+    formValidator()
+  }
 
   return (
-    <form>
+    <form method='POST'>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
       <input
@@ -23,7 +39,8 @@ export default function FormValidator () {
         type='password' name='password-confirm'
         onChange={e => setPasswordConfirm(e.target.value)}
       />
-      <input type='submit' value='Submit' />
+      <p>{messages.join(", ")}</p>
+      <input type='submit' value='Submit' onClick={validateForm} />
     </form>
   )
 }
