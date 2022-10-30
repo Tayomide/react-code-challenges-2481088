2ab51e3c3e5b6aff286069ca7de5de0335660fa2
Tayomide <tibrahim@hawk.iit.edu>
2022-10-30 17:57:50 -0500

    Solve problem six of React Code Challenges

--- src/06-add-window-event/ToggleWindowEvent.js
+++ src/06-add-window-event/ToggleWindowEvent.js
@@ -1,8 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import WindowEvent from './WindowEvent'
 
 export default function ToggleWindowEvent () {
   const [windowEvent, setWindowEvent] = useState(false)
+  const event = () => {
+    window.alert("mouse pressed")
+  }
+  useEffect(() => {
+    if(windowEvent)window.addEventListener("dblclick", event)
+    return () => {
+      window.removeEventListener("dblclick", event)
+    }
+  }, [windowEvent])
   return (
     <div>
       <button onClick={() => setWindowEvent(prevState => !prevState)}>Toggle Window Event</button>
