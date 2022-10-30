dc89f2c9f2612828683e7dec503c8bafb1554b16
Tayomide <tibrahim@hawk.iit.edu>
2022-10-30 18:35:31 -0500

    Solve problem seven of React Code Challenges

--- src/07-color-picker/Color.js
+++ src/07-color-picker/Color.js
@@ -1,8 +1,9 @@
-export default function Color ({ hex, name }) {
+export default function Color ({ hex, name, changeBackground }) {
   return (
     <button
       className='color-square'
       style={{ backgroundColor: hex }}
+      onClick={() => changeBackground(hex)}
     >
       <h2>{name}</h2>
     </button>
--- src/07-color-picker/ColorPicker.js
+++ src/07-color-picker/ColorPicker.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Color from './Color'
 
 const colors = [{
@@ -21,11 +21,15 @@ const colors = [{
 export default function ColorPicker () {
   const [backgroundColor, setBackgroundColor] = useState('white')
 
+  const changeBackground = (color) => {
+    setBackgroundColor(color)
+  }
+
   return (
     <div className='page' style={{ backgroundColor }}>
       {
         colors.map(color => (
-          <Color key={color.hex} hex={color.hex} name={color.name} />
+          <Color key={color.hex} hex={color.hex} name={color.name} changeBackground={changeBackground}/>
         ))
       }
     </div>
