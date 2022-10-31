6733b5e9a498c5c428a796bc458129ec71801c85
Tayomide <tibrahim@hawk.iit.edu>
2022-10-30 19:58:41 -0500

    Update problem eight solution of React Code Challenges

--- src/08-pixel-art/PixelArt.js
+++ src/08-pixel-art/PixelArt.js
@@ -1,20 +1,24 @@
 import { createContext, useState, useContext } from 'react'
-const ColorContext = createContext();
+const ColorContext = createContext({
+  currentColor: "lightGrey",
+  setCurrentColor: () => {}
+});
 
-function ColorPicker ({setCurrentColor}) {
+function ColorPicker () {
   const colors = ['red', 'blue', 'yellow', 'green', 'black', 'white', 'purple']
+  const globalColor = useContext(ColorContext)
   return (
     <div>
       <h1>Choose a color</h1>
-      {colors.map(color => <button key={color} style={{ backgroundColor: color }} onClick={() => setCurrentColor(color)}/>)}
+      {colors.map(color => <button key={color} style={{ backgroundColor: color }} onClick={() => globalColor.setCurrentColor(color)}/>)}
     </div>
   )
 }
 
 function Pixel () {
   const [background, setBackground] = useState("lightGrey")
-  const color = useContext(ColorContext);
-  return <div onClick={() => setBackground(color)} style={{ height: '20px', width: '20px', backgroundColor: `${background}`, margin: '1px' }} />
+  const globalColor = useContext(ColorContext);
+  return <div onClick={() => setBackground(globalColor.currentColor)} style={{ height: '20px', width: '20px', backgroundColor: `${background}`, margin: '1px' }} />
 }
 
 function Pixels () {
@@ -30,8 +34,8 @@ function Pixels () {
 export default function PixelArt () {
   const [currentColor, setCurrentColor] = useState("lightGrey")
   return (
-    <ColorContext.Provider value={currentColor} >
-      <ColorPicker setCurrentColor={setCurrentColor}/>
+    <ColorContext.Provider value={{currentColor, setCurrentColor}} >
+      <ColorPicker/>
       <Pixels />
     </ColorContext.Provider>
   )
